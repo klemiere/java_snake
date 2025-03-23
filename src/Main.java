b149f31371354185fc5b1dabdb3d8a29183ec527
klemiere <lemierekevin1@gmail.com>
2025-03-23 20:12:24 +0100

    Initial commit

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-    GUI GameWindow = new GUI();
-    GameObjects.Grid grid = new GameObjects.Grid();
-        System.out.println("Rows: " + grid.rows);
-    }
-}
